fd7db6a93ce0f879e138b6c777c076436dbfc0f9
kiksgold <kiksgold@gmail.com>
2022-12-14 23:20:37 +0000

    Create functions for the battleship game

--- run.py
+++ run.py
@@ -23,3 +23,19 @@ HIDDEN_GRID = [[''] * 8 for x in range(8)]
 GUESS_GRID = [[''] * 8 for x in range(8)]
 
 let_to_num = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
+
+def board():
+    pass
+
+def create_ships():
+    pass
+
+def get_ship_location():
+    pass
+
+def count_hit_ships():
+    pass
+
+
+
+
