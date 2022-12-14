a363f816c4a8f68e6fea51853341cc90fa575d02
kiksgold <kiksgold@gmail.com>
2022-12-14 12:47:46 +0000

    Initial Commit

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+    -------BATTLESHIPS 101------
+    How it will work:
+    1. A 8x18 grid will have 5 ships of variable length randomly placed about
+    2. You will have 50 bullets to take down the ships that are placed down
+    3. You can choose a row and column such as A3 to indicate where to shoot
+    4. For every shot that hits or misses it will show up in the grid
+    5. A ship cannot be placed diagonally, so if a shot hits the rest of
+        the ship is in one of 4 directions, left, right, up, and down
+    6. If all ships are unearthed before using up all bullets, you win
+        else, you lose and it's game over!
+
+    Legend
+    'X' indicates the ships hit
+    '-' indicates the hits missed
+    '' indicates available space        
+
+"""
