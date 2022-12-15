0827868dee1aebacc094c16b15898e49c96381be
kiksgold <kiksgold@gmail.com>
2022-12-15 07:38:47 +0000

    Define function to print the grid size

--- run.py
+++ run.py
@@ -24,8 +24,14 @@ GUESS_GRID = [[''] * 8 for x in range(8)]
 
 let_to_num = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
 
-def board():
-    pass
+# Define Function to Print Battleship grid size
+def print_grid(grid):
+    print(' A B C D E F G H')
+    print(' ---------------')
+    row_num = 1
+    for row in grid:
+        print("%d|%s|" % (row_num, "|".join(row)))
+        row_num += 1
 
 def create_ships():
     pass
