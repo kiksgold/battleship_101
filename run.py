6bb8716ef36f645680027744bf8547e0b8fee8ee
kiksgold <kiksgold@gmail.com>
2022-12-15 08:06:16 +0000

    Define Function to count hit ships

--- run.py
+++ run.py
@@ -3,9 +3,9 @@ from random import randint
 # """
 #     -------BATTLESHIPS 101------
 #     How it will work:
-#     1. A 8x8 grid will have 5 ships of variable length randomly placed about
+#     1. A 10x10 grid will have 5 ships of variable length randomly placed about
 #     2. You will have 50 bullets to take down the ships that are placed down
-#     3. You can choose a row and column such as A3 to indicate where to shoot
+#     3. You can choose a row and column such as B5 to indicate where to shoot
 #     4. For every shot that hits or misses it will show up in the grid
 #     5. A ship cannot be placed diagonally, so if a shot hits the rest of
 #         the ship is in one of 4 directions, left, right, up, and down
@@ -19,11 +19,11 @@ from random import randint
 # """
 
 # Grid for holding ship locations
-HIDDEN_GRID = [[''] * 8 for x in range(8)]
+HIDDEN_GRID = [[''] * 10 for x in range(10)]
 # Grid for displaying hits and miss
-GUESS_GRID = [[''] * 8 for x in range(8)]
+GUESS_GRID = [[''] * 10 for x in range(10)]
 
-let_to_num = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
+let_to_num = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9}
 
 
 # Define Function to Print Battleship grid size
@@ -39,30 +39,36 @@ def print_grid(grid):
 # Define Function to create the ships
 def create_ships(grid):
     for ship in range(5):
-        ship_row, ship_column = randint(0, 7), randint(0, 7)
+        ship_row, ship_column = randint(0, 9), randint(0, 9)
         while grid[ship_row][ship_column] == 'X':
-            ship_row, ship_column = randint(0, 7), randint(0, 7)
+            ship_row, ship_column = randint(0, 9), randint(0, 9)
         grid[ship_row][ship_column] = 'X'
 
 
 # Define Function to get ship location
 def get_ship_location():
-    # Enter the row number between 1 to 8
-    row = input('Please enter a ship row 1-8: ')
+    # Enter the row number between 1 to 10
+    row = input('Please enter a ship row 1-10: ')
     while row not in '12345678':
         print("Please enter a valid row")
-        row = input('Please enter a ship row 1-8: ')
+        row = input('Please enter a ship row 1-10: ')
 
-    # Enter the Ship column from A TO H
-    column = input('Please enter a ship column A-H: ').upper()
+    # Enter the Ship column from A TO J
+    column = input('Please enter a ship column A-J: ').upper()
     while column not in 'ABCDEFGH':
         print("Please enter a valid column")
-        column = input('Please enter a ship column A-H: ').upper()
+        column = input('Please enter a ship column A-J: ').upper()
     return int(row) -1, let_to_num[column]
 
 
-def count_hit_ships():
-    pass
+# Define Function to count hit ships
+def count_hit_ships(board):
+    count = 0
+    for row in board:
+        for column in row:
+            if column == 'X':
+                count += 1
+    return count
 
 
 
