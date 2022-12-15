a78abd8ea40b90671dd83a5c676ad5a704c8a6b7
kiksgold <kiksgold@gmail.com>
2022-12-15 07:52:49 +0000

    Define function to create ships

--- run.py
+++ run.py
@@ -1,21 +1,22 @@
+from random import randint
 
-"""
-    -------BATTLESHIPS 101------
-    How it will work:
-    1. A 8x8 grid will have 5 ships of variable length randomly placed about
-    2. You will have 50 bullets to take down the ships that are placed down
-    3. You can choose a row and column such as A3 to indicate where to shoot
-    4. For every shot that hits or misses it will show up in the grid
-    5. A ship cannot be placed diagonally, so if a shot hits the rest of
-        the ship is in one of 4 directions, left, right, up, and down
-    6. If all ships are unearthed before using up all bullets, you win
-        else, you lose and it's game over!
+# """
+#     -------BATTLESHIPS 101------
+#     How it will work:
+#     1. A 8x8 grid will have 5 ships of variable length randomly placed about
+#     2. You will have 50 bullets to take down the ships that are placed down
+#     3. You can choose a row and column such as A3 to indicate where to shoot
+#     4. For every shot that hits or misses it will show up in the grid
+#     5. A ship cannot be placed diagonally, so if a shot hits the rest of
+#         the ship is in one of 4 directions, left, right, up, and down
+#     6. If all ships are unearthed before using up all bullets, you win
+#         else, you lose and it's game over!
 
-    Legend:
-    'X' indicates the ships hit
-    '-' indicates the hits missed
-    '' indicates available space
-"""
+#     Legend:
+#     'X' indicates the ships hit
+#     '-' indicates the hits missed
+#     '' indicates available space
+# """
 
 # Grid for holding ship locations
 HIDDEN_GRID = [[''] * 8 for x in range(8)]
@@ -23,6 +24,7 @@ HIDDEN_GRID = [[''] * 8 for x in range(8)]
 GUESS_GRID = [[''] * 8 for x in range(8)]
 
 let_to_num = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
+
 
 # Define Function to Print Battleship grid size
 def print_grid(grid):
@@ -33,8 +35,14 @@ def print_grid(grid):
         print("%d|%s|" % (row_num, "|".join(row)))
         row_num += 1
 
-def create_ships():
-    pass
+
+# Define Function to create the ships
+def create_ships(grid):
+    for ship in range(5):
+        ship_row, ship_column = randint(0, 7), randint(0, 7)
+        while board[ship_row][ship_column] == 'X':
+            ship_row, ship_column = randint(0, 7), randint(0, 7)
+        board[ship_row][ship_column] = 'X'
 
 def get_ship_location():
     pass
